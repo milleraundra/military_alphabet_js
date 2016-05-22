05d25d460861730707e226cc691d40fa750cdc65
Aundra Miller <miller.aundra@gmail.com>
2016-05-22 12:48:06 -0700

    begin structure for military_alpabet function.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,44 @@
+function militaryAlphabet(input) {
+	var input_uppercase = toUpperCase(input);
+	var result = "";
+	var military_alphabet = array(
+			'A' => 'Alpha',
+			'B' => 'Bravo',
+			'C' => 'Charlie',
+			'D' => 'Delta', 
+			'E' => 'Echo',
+			'F' => 'Foxtrot',
+			'G' => 'Golf',
+			'H' => 'Hotel',
+			'I' => 'India',
+			'J' => 'Juliet',
+			'K' => 'Kilo',
+			'L' => 'Lima',
+			'M' => 'Mike',
+			'N' => 'November',
+			'O' => 'Oscar',
+			'P' => 'Papa',
+			'Q' => 'Quebec',
+			'R' => 'Romeo',
+			'S' => 'Sierra',
+			'T' => 'Tango',
+			'U' => 'Uniform',
+			'V' => 'Victor',
+			'W' => 'Whiskey',
+			'X' => 'X-ray',
+			'Y' => 'Yankee',
+			'Z' => 'Zulu'
+		);
+	//validate
+	if (input)
 
+	//take sentence with multiple words
+	//loop through letters
+		//if a letter, return assocaited
+		//if a space, '<br>'
+	//push to final array
+	//return array
+}
 
 
 
